78f9c696fb20304f589921fea9adac8323bb2ed9
Emil Babazade <emil.babazade@atea.com>
2025-10-20 15:44:21 +0300

    checkpoint

--- CommonFilters/DataAnnotationsValidationFilter.cs
+++ CommonFilters/DataAnnotationsValidationFilter.cs
@@ -2,4 +2,26 @@ namespace Shopify.CommonFilters;
 
 public sealed class DataAnnotationsValidationFilter<T> : IEndPointFilter<T> where T : class
 {
+    public async ValueTask<object> InvokeAsync(
+        EndpointFilterInvocationContext c, EndpointFilterDelegate next)
+    {
+        var model = c.Arguments.OfType<T>().FirstOrDefault();
+        if (model is null) return Results.BadRequest("Invalid body.");
+
+        var results = new List<ValidationResult>();
+        var ctx = new ValidationContext(model);
+        var ok = Validator.TryValidateObject(model, ctx, results, true);
+
+        if (!ok)
+        {
+            var errors = results
+                .GroupBy(r => r.MemberNames.FirstOrDefault() ?? string.Empty)
+                .ToDictionary(g => g.Key, g => g.Select(r => r.ErrorMessage!).ToArray());
+
+            return Results.ValidationProblem(errors);
+        }
+
+        return await next(c);
+    }
+
 }
--- Shopify.UsersAPI/Program.cs
+++ Shopify.UsersAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Shopify.UsersAPI.Models;
 using Shopify.UsersAPI.RequestBodies;
@@ -21,11 +22,13 @@ if (app.Environment.IsDevelopment())
 
 // TODO: validation error handling common filter
 var group = app.MapGroup("/users");
-group.MapPost("/register", async (
-    [FromBody] UserRequestBody userRequestBody,
-    UsersService usersService,
-    HttpContext httpContext,
-    CancellationToken cancellationToken
-) => usersService.CreateUser(userRequestBody, cancellationToken));
+group
+    .MapPost("/register", async (
+        [FromBody] UserRequestBody userRequestBody,
+        UsersService usersService,
+        HttpContext httpContext,
+        CancellationToken cancellationToken
+    ) => usersService.CreateUser(userRequestBody, cancellationToken))
+    .AddEndpointFilter<DataAnnotationsValidationFilter>();
 
 app.Run();
